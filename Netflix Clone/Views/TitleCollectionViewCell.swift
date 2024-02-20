3f04f283ff9e0ce890b7eaf5e0228487f31efab1
Muhammad Younis <Muhammad.gamal2@icloud.com>
2024-02-21 00:16:11 +0200

    Create Custom CollectionViewCell to Show Image Passing the data to the CollectionViewCell

---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by muhammed gamal on 20/02/2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
